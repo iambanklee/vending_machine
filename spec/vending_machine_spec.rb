169b1bdca21e639fc95d31a7e939ec37a5b97638
Bank Lee <iambanklee@gmail.com>
2020-05-15 11:19:18 +0100

    requirements in RSpec context and examples

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,34 @@
+require 'vending_machine'
+
+RSpec.describe VendingMachine do
+  context 'The machine should take an initial load of products and change. The change will be of denominations 1p, 2p, 5p, 10p, 20p, 50p, £1, £2' do
+
+  end
+
+  context 'There should be a way of reloading either products or change at a later point' do
+
+  end
+
+  context 'The machine should keep track of the products and change that it contains'
+
+  context 'given an item is selected' do
+    context 'when the appropriate amount of money is inserted' do
+      it 'the vending machine should return the correct product' do
+      end
+    end
+
+    context 'when inserted money more than than product price' do
+
+      it'It should also return change if too much money is provided' do
+
+      end
+    end
+
+    context 'when insufficient funds have been inserted' do
+      it'It should ask for more' do
+
+      end
+    end
+  end
+
+end
