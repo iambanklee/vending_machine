cdea91b88145bba728470dfd6f110d1ba3afd00d
Bank Lee <iambanklee@gmail.com>
2020-05-16 14:18:58 +0100

    initial implementation

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,2 +1,60 @@
 class VendingMachine
+  attr_reader :item_inventory, :change_inventory, :inserted_money
+
+  CHANGE_DENOMINATION_MAP = {
+      '£2' => 200,
+      '£1' => 100,
+      '50p' => 50,
+      '20p' => 20,
+      '10p' => 10,
+      '5p' => 5,
+      '2p' => 2,
+      '1p' => 1,
+  }
+
+  def initialize(item, change)
+    @item_inventory = JSON.parse(item)
+    @change_inventory = JSON.parse(change)
+    @inserted_money = 0
+    @order_price = 0
+  end
+
+  def purchase(item_name)
+    select_item(item_name)
+
+    while outstanding_money > 0
+      coin = Kernel.gets.chomp
+      insert_money(coin)
+    end
+
+    item_name
+  end
+
+  def return_changes(amount)
+    result = []
+
+    CHANGE_DENOMINATION_MAP.each do |denomination, pence|
+      next if pence > amount
+      while amount > 0
+        result << denomination
+        amount -= pence
+        break if pence > amount
+      end
+    end
+
+    result
+  end
+
+  def select_item(name)
+    product = item_inventory[name]
+    @order_price += product['price']
+  end
+
+  def insert_money(coin)
+    @inserted_money += CHANGE_DENOMINATION_MAP[coin]
+  end
+
+  def outstanding_money
+    @order_price - @inserted_money
+  end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,34 +1,109 @@
 require 'vending_machine'
 
 RSpec.describe VendingMachine do
-  context 'The machine should take an initial load of products and change. The change will be of denominations 1p, 2p, 5p, 10p, 20p, 50p, £1, £2' do
+  let(:vending_machine) { described_class.new(items, changes) }
+
+  let(:items) do
+    {
+      "Green Tea": {
+        price: 79,
+        stock: 10
+      },
+      "Milk Tea": {
+        price: 99,
+        stock: 10
+      },
+      "Black Tea": {
+        price: 59,
+        stock: 10
+      },
+    }.to_json
+  end
+
+  let(:changes) do
+    {
+      '1p': 10,
+      '2p': 10,
+      '5p': 10,
+      '10p': 10,
+      '20p': 10,
+      '50p': 10,
+      '£1': 10,
+      '£2': 10,
+    }.to_json
+  end
+
+  xcontext 'There should be a way of reloading either products or change at a later point' do
 
   end
 
-  context 'There should be a way of reloading either products or change at a later point' do
+  xcontext 'The machine should keep track of the products and change that it contains'
 
-  end
+  describe '#select_item' do
+    subject { vending_machine.select_item(name) }
 
-  context 'The machine should keep track of the products and change that it contains'
+    context 'when item exist and has stock' do
+      let(:name) { 'Green Tea'}
 
-  context 'given an item is selected' do
-    context 'when the appropriate amount of money is inserted' do
-      it 'the vending machine should return the correct product' do
-      end
-    end
-
-    context 'when inserted money more than than product price' do
-
-      it'It should also return change if too much money is provided' do
-
-      end
-    end
-
-    context 'when insufficient funds have been inserted' do
-      it'It should ask for more' do
-
+      it 'returns the order price' do
+        is_expected.to eq(79)
       end
     end
   end
 
+  describe '#insert_money' do
+    subject { vending_machine.insert_money(coin) }
+
+    let(:coin) { '50p' }
+
+    it 'return inserted money' do
+      is_expected.to eq(50)
+    end
+  end
+
+  describe '#outstanding_money' do
+    subject { vending_machine.outstanding_money }
+
+    context 'when appropriate amount of money is inserted' do
+      it 'returns zero' do
+        is_expected.to eq(0)
+      end
+    end
+  end
+
+  describe '#purchase' do
+    subject { vending_machine.purchase(item_name) }
+
+    context 'given an item is selected' do
+      let(:item_name) { 'Green Tea' }
+
+      context 'when the appropriate amount of money is inserted' do
+        it 'the vending machine should return the correct product' do
+          expect(Kernel).to receive(:gets).and_return('50p', '50p')
+          expect(vending_machine).to receive(:outstanding_money).and_return(79, 29, -21)
+
+          is_expected.to eq('Green Tea')
+        end
+      end
+    end
+  end
+
+  describe '#return_changes' do
+    subject { vending_machine.return_changes(amount) }
+
+    context '79p' do
+      let(:amount) { 79 }
+      it { is_expected.to eq(%w(50p 20p 5p 2p 2p)) }
+    end
+
+    context '21p' do
+      let(:amount) { 21 }
+      it { is_expected.to eq(%w(20p 1p)) }
+    end
+
+    context '128p' do
+      let(:amount) { 128 }
+      it { is_expected.to eq(%w(£1 20p 5p 2p 1p)) }
+    end
+  end
 end
