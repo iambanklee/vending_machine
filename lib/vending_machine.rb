5d1334b58bf7617e93ce3bbe48c822a97a093e16
Bank Lee <iambanklee@gmail.com>
2020-05-18 13:01:47 +0100

    Replace private methods for better readability

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -42,7 +42,8 @@ class VendingMachine
     end
 
     puts "Please collect your #{select_item.name}"
-    deduct_item_inventory(select_item)
+
+    item_inventory.decrease(name: select_item.name, stock: 1)
     puts "#{select_item.name} now has #{item_inventory.stock_of(select_item.name)} in stock"
 
     return_change
@@ -53,7 +54,7 @@ class VendingMachine
   def return_change
     if outstanding_amount < 0
       change_detail = amount_to_change(-outstanding_amount)
-      deduct_change_inventory(change_detail)
+      change_detail.each { |change| change_inventory.decrease(name: change, stock: 1) }
       puts "Please remember to collect your change: #{change_detail}"
     end
   end
@@ -74,16 +75,6 @@ class VendingMachine
   end
 
   private
-
-  def deduct_item_inventory(item)
-    item_inventory.items[item.name] -= 1
-  end
-
-  def deduct_change_inventory(change_detail)
-    change_detail.each do |coin|
-      change_inventory.items[coin] -= 1
-    end
-  end
 
   def select_item(name)
     product = products[name]
